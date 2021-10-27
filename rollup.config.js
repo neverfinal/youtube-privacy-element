c3e18263cbdc57a5157469555937ffc251921dc0
Raimund Canzler <git@rai-canzler.de>
2021-10-27 02:18:52 +0200

    Unify formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,52 @@
 const postcss = require('rollup-plugin-postcss');
-const {babel} = require('@rollup/plugin-babel');
+const { babel } = require('@rollup/plugin-babel');
 const { terser } = require('rollup-plugin-terser');
 
 const plugins = [
-    postcss({
-        inject: false,
-        extract: false,
-    }),
-    babel({
-        "presets": [[
-            "@babel/preset-env",
-            {
+	postcss({
+		inject: false,
+		extract: false,
+	}),
+	babel({
+		presets: [
+			[
+				'@babel/preset-env',
+				{
+					targets: {
+						edge: '17',
+						firefox: '60',
+						chrome: '67',
+						safari: '11.1',
+					},
+				},
+			],
+		],
 		plugins: [
 			'@babel/plugin-proposal-class-properties',
 			'@babel/plugin-transform-classes',
 		],
-    }),
-    terser(),
-]
+	}),
+	terser(),
+];
 
 const esBundle = {
-    input: './src/es.js',
-    output: {
-        dir: `dist`,
-        entryFileNames: 'youtube-privacy.es.js',
-        format: 'es'
-    },
-    plugins,
+	input: './src/es.js',
+	output: {
+		dir: `dist`,
+		entryFileNames: 'youtube-privacy.es.js',
+		format: 'es',
+	},
+	plugins,
 };
 
 const iifeBundle = {
-    input: './src/iife.js',
-    output: {
-        dir: `dist`,
-        entryFileNames: 'youtube-privacy.js',
-        format: 'iife'
-    },
-    plugins,
+	input: './src/iife.js',
+	output: {
+		dir: `dist`,
+		entryFileNames: 'youtube-privacy.js',
+		format: 'iife',
+	},
+	plugins,
 };
 
-export default [ esBundle, iifeBundle ];
+export default [esBundle, iifeBundle];
