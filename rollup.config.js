a1280df7c90cee4ba14ac07152fa2ba17a602b24
Raimund Canzler <git@rai-canzler.de>
2021-10-27 13:45:26 +0200

    Remove babel build step

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,10 @@
 const postcss = require('rollup-plugin-postcss');
-const {babel} = require('@rollup/plugin-babel');
 const { terser } = require('rollup-plugin-terser');
 
 const plugins = [
     postcss({
         inject: false,
         extract: false,
-    }),
-    babel({
-        "presets": [[
-            "@babel/preset-env",
-            {
-                "targets": {
-                    "edge": "17",
-                    "firefox": "60",
-                    "chrome": "67",
-                    "safari": "11.1",
-                }
-            }
-        ]],
     }),
     terser(),
 ]
